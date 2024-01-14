4ae5a3bae07afad6a7a8bc7c217377edefc5eb19
andrefcosta-digital <hello@andrefcosta.com>
2024-01-14 12:00:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+openai.api_key  = "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"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = get_completion(userText)
+    #return str(bot.get_response(userText))
+    return response
+
+if __name__ == "__main__":
+  app.run()
